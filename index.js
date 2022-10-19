fe69ef0fb9a7f5a16f51de667a4480ac2a40ba38
Guilherme Santana <guisss02@hotmail.com>
2022-10-19 11:31:30 -0300

    Tela de erro adicionada

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var IsLastOperation = true;
 var inputVisual = document.getElementById("inputresultado");
+var IsOnError = false;
 
 function noRepeat(givenid) {
     if (["sum", "minus", "multiplication", "division"].includes(givenid)) {
@@ -10,154 +11,165 @@ function noRepeat(givenid) {
     }
 }
 function tap(givenid) {
-    if (!isNaN(parseInt(givenid))) {
-        inputVisual.value += givenid;
-        this.IsLastOperation = false;
-    }
 
+    if (givenid == "clear") {
+        inputVisual.value = ""
+        this.IsLastOperation = true;
+        this.IsOnError = false;
+        inputVisual.style.color = "black"
+    }
     else {
 
-        switch (givenid) {
-            case "sum":
-
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "+";
+        try {
+            if (!this.IsOnError) {
+                if (!isNaN(parseInt(givenid))) {
+                    inputVisual.value += givenid;
+                    this.IsLastOperation = false;
                 }
 
-                noRepeat(givenid);
-                break;
+                else {
+
+                    switch (givenid) {
+                        case "sum":
+
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "+";
+                            }
+
+                            noRepeat(givenid);
+                            break;
 
 
-            case "minus":
+                        case "minus":
 
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "-";
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "-";
+                            }
+
+                            noRepeat(givenid);
+                            break;
+
+                        case "multiplication":
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "*";
+                            }
+
+                            noRepeat(givenid);
+
+                            break;
+
+                        case "division":
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "/";
+                            }
+                            noRepeat(givenid);
+
+                            break;
+
+                        case "module":
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "%";
+                            }
+                            noRepeat(givenid);
+                            break;
+
+                        case "square":
+                            if (this.IsLastOperation == false) {
+                                inputVisual.value += "**";
+                            }
+                            noRepeat(givenid);
+                            break;
+
+                        case "result":
+                            result = eval(inputVisual.value);
+                            inputVisual.value = result;
+                            noRepeat();
+                            break;
+
+                        case "leftparentesis":
+                            inputVisual.value += "(";
+                            noRepeat();
+
+                            break;
+
+                        case "rightparentesis":
+                            inputVisual.value += ")";
+                            noRepeat();
+
+                            break;
+
+                        case "erase":
+                            let inputV = inputVisual.value
+                            inputVisual.value = inputVisual.value.substring((0), (inputVisual.value.length - 1))
+                            noRepeat();
+
+                            break;
+                    }
+
                 }
+            }
 
-                noRepeat(givenid);
-                break;
+            document.addEventListener("keydown", function (e) {
+                console.log(e.key);
+            })
 
-            case "multiplication":
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "*";
+            document.addEventListener("keydown", function (e) {
+                if (!isNaN(e.key)) {
+                    tap(e.key)
                 }
+                else {
+                    switch (e.key) {
+                        case "+":
+                            tap("sum")
+                            e.preventDefault();
+                            break;
 
-                noRepeat(givenid);
+                        case "-":
+                            tap("minus")
+                            break;
 
-                break;
+                        case "%":
+                            tap("module")
+                            break;
 
-            case "division":
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "/";
+                        case "(":
+                            tap("leftparentesis")
+                            break;
+
+                        case ")":
+                            tap("rightparentesis")
+                            break;
+
+                        case "Backspace":
+                            tap("erase")
+                            break;
+
+                        case "c":
+                            tap("clear")
+                            break;
+
+                        case ")":
+                            tap("rightparentesis")
+                            break;
+                        case "*":
+                            tap("multiplication")
+                            break;
+                        case "/":
+                            tap("division")
+                            break;
+
+                        case "Enter":
+                            tap("result")
+                            break;
+
+                    }
                 }
-                noRepeat(givenid);
-
-                break;
-
-            case "module":
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "%";
-                }
-                noRepeat(givenid);
-                break;
-
-            case "square":
-                if (this.IsLastOperation == false) {
-                    inputVisual.value += "**";
-                }
-                noRepeat(givenid);
-                break;
-
-            case "clear":
-                inputVisual.value = ""
-                this.IsLastOperation = true;
-                break;
-
-            case "result":
-                result = eval(inputVisual.value);
-                inputVisual.value = result;
-                noRepeat();
-                break;
-
-            case "leftparentesis":
-                inputVisual.value += "(";
-                noRepeat();
-
-                break;
-
-            case "rightparentesis":
-                inputVisual.value += ")";
-                noRepeat();
-
-                break;
-
-            case "erase":
-                let inputV = inputVisual.value
-                inputVisual.value = inputVisual.value.substring((0), (inputVisual.value.length - 1))
-                noRepeat();
-
-                break;
+            })
         }
-
+        catch (error) {
+            inputVisual.value = "ERR0R";
+            inputVisual.style.color = "red";
+            this.IsOnError = true;
+        }
     }
 }
-
-document.addEventListener("keydown", function (e) {
-    console.log(e.key);
-})
-
-document.addEventListener("keydown", function (e) {
-    if (!isNaN(e.key)) {
-        tap(e.key)
-    }
-    else
-    {
-        switch (e.key) {
-            case "+":
-                tap("sum")
-                e.preventDefault();
-                break;
-
-            case "-":
-                tap("minus")
-                break;
-
-            case "%":
-                tap("module")
-                break;
-
-            case "(":
-                tap("leftparentesis")
-                break;
-
-            case ")":
-                tap("rightparentesis")
-                break;
-
-            case "Backspace":
-                tap("erase")
-                break;
-
-            case "c":
-                tap("clear")
-                break;
-
-            case ")":
-                tap("rightparentesis")
-                break;
-            case "*":
-                tap("multiplication")
-                break;
-            case "/":
-                tap("division")
-                break;
-
-            case "Enter":
-                tap("result")
-                break;
-
-            }
-    }
-})
-
-
